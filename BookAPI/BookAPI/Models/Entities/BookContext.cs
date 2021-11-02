39ac1b6a820501e1f3a82c98a957b923d5ad09f8
Luiz Carlos <lewiscontato99@gmail.com>
2021-11-02 18:26:27 -0300

    Created a project BookAPI

---
+++ BookAPI/BookAPI/Models/Entities/BookContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAPI.Models.Entities
+{
+    public class BookContext:DbContext
+    {
+        public DbSet<Book> Book { get; set; }
+
+
+        public BookContext(DbContextOptions<BookContext> options ):base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+    }
+}
